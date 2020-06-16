45229d9b29e17326009fd5e8e9fd392df0d9548a
kobeomseok <kobeomseok@DESKTOP-O5DQE4O>
2020-06-17 02:35:03 +0900

    200617_02:34_범석브랜치 커밋

---
+++ haemukjaProject/src/qna/controller/QnaChangeCommentServlet.java
@@ -0,0 +1,67 @@
+package qna.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import qna.model.service.QnaService;
+import qna.model.vo.Comment;
+
+@WebServlet("/changeComment.qn")
+public class QnaChangeCommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//updateComment, updateReply 남음
+		String actionType = request.getParameter("actionType");
+		int qid = Integer.valueOf(request.getParameter("qid"));
+		QnaService qs = new QnaService();
+		int result = 0;
+		
+		if(actionType.equals("addComment") || actionType.equals("addReply")) {
+			String qComment = request.getParameter("qComment");
+			String mid = request.getParameter("mid");
+			int parentNo = Integer.valueOf(request.getParameter("parentNo"));
+			int orderNo = Integer.valueOf(request.getParameter("orderNo"));
+			int groupNo = Integer.valueOf(request.getParameter("groupNo"));
+			int depth = Integer.valueOf(request.getParameter("depth"));
+			
+			Comment c = new Comment();
+			c.setQid(qid);
+			c.setqComment(qComment);
+			c.setmNickname(mid);	//아이디입니다.
+			c.setParentNo(parentNo);
+			c.setOrderNo(orderNo);
+			c.setGroupNo(groupNo);
+			c.setDepth(depth);
+			
+			result = qs.insertComment(c);
+		} 
+		else if(actionType.equals("deleteComment") || actionType.equals("deleteReply")) {
+			int qcno = Integer.valueOf(request.getParameter("qcno"));
+			result = qs.deleteComment(qcno);
+		}
+		
+		
+		//댓글들 다시 조회
+		ArrayList<Comment> commentList = qs.selectReplyList(qid);
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+		gson.toJson(commentList, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
